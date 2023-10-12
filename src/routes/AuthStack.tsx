314db20fd1b1fa7a81976004dd2f70da6299a396
Isaque Akamine <isaqueakamine@gmail.com>
2023-10-12 00:00:56 -0300

    fix: added initial route

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+// import {View, Text} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Login, SignUp} from '../screens';
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+// function HomeScreen() {
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
-      <Stack.Screen name="Home" component={HomeScreen} />
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
     </Stack.Navigator>
   );
 }
