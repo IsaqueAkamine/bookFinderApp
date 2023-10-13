552caab9c9bef3c676f6b160f344646213a73b07
Isaque Akamine <isaqueakamine@gmail.com>
2023-10-12 23:03:20 -0300

    feat: create login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import Input from '../../components/Input';
 
-import {Container, Title} from './styles';
+import {
+  ButtonContainer,
+  Container,
+  ForgotContainer,
+  ForgotText,
+  FormContainer,
+  Image,
+  NoAccountContainer,
+  SafeAreaContainer,
+  SignUpContainer,
+  SignUpText,
+  Title,
+} from './styles';
+import Button from '../../components/Button';
 
 const Login: React.FC = () => {
+  const handleForgot = () => {
+    console.log('forgot');
+  };
+
+  const handleSignUp = () => {
+    console.log('signup');
+  };
+
   return (
     <Container>
-      <Title>Login Screen</Title>
+      <Image
+        source={require('../../assets/img/books.jpg')}
+        resizeMode="stretch"
+      />
+      <SafeAreaContainer>
+        <FormContainer>
+          <Title>HELLO BOOK</Title>
+          <Input
+            description="username"
+            maxLength={40}
+            keyboardType="email-address"
+          />
+          <Input description="password" isPassword maxLength={40} />
+
+          <ForgotContainer onPress={handleForgot}>
+            <ForgotText>Forgot password?</ForgotText>
+          </ForgotContainer>
+
+          <ButtonContainer>
+            <Button title="Sign In" />
+          </ButtonContainer>
+
+          <NoAccountContainer>
+            <SignUpText>No Account yet? </SignUpText>
+            <SignUpContainer onPress={handleSignUp}>
+              <SignUpText>Sign Up</SignUpText>
+            </SignUpContainer>
+          </NoAccountContainer>
+        </FormContainer>
+      </SafeAreaContainer>
     </Container>
   );
 };
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,4 +1,73 @@
 import styled from 'styled-components/native';
+import {COLORS} from '../../constants';
 
-export const Container = styled.View``;
-export const Title = styled.Text``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${COLORS.background};
+`;
+
+export const SafeAreaContainer = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 50%;
+`;
+
+export const FormContainer = styled.View`
+  flex: 1;
+  margin-top: -70px;
+  background-color: ${COLORS.background}90;
+  border-radius: 30px;
+  padding: 0 20px;
+  gap: 10px;
+`;
+
+export const Title = styled.Text`
+  height: 70px;
+  text-align: center;
+  line-height: 70px;
+  font-size: 36px;
+  color: ${COLORS.primary};
+  font-weight: bold;
+`;
+
+export const ForgotContainer = styled.Pressable`
+  margin-top: 10px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${COLORS.secondary};
+  padding-bottom: 3px;
+  align-items: flex-start;
+  align-self: flex-start;
+`;
+
+export const ForgotText = styled.Text`
+  color: ${COLORS.secondary};
+  font-weight: 500;
+`;
+
+export const ButtonContainer = styled.View`
+  /* flex: 1; */
+  padding: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NoAccountContainer = styled.View`
+  flex-direction: row;
+  /* margin-bottom: 20px; */
+`;
+
+export const SignUpContainer = styled.Pressable`
+  border-bottom-width: 1px;
+  border-bottom-color: ${COLORS.secondary};
+  padding-bottom: 3px;
+  align-items: flex-start;
+  align-self: flex-start;
+`;
+
+export const SignUpText = styled.Text`
+  color: ${COLORS.secondary};
+  font-weight: bold;
+`;
